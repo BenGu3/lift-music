cb7f0d834390964a354173ea939fd75f6d4d8426
Ben Guthrie <ben-guthrie@pluralsight.com>
2019-12-24 19:50:21 -0800

    update webpack config to add env vars in local dev

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,47 @@ const Dotenv = require('dotenv-webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
-module.exports = (env) => ({
-  mode: 'development',
-  entry: {
-    app: './src/index.js'
-  },
-  plugins: [
+module.exports = (env) => {
+  const plugins = [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './index.local.html'
-    }),
-    new Dotenv({
-      path: env && env.NODE_ENV === 'production' ? './.env' : './.env.dev'
     })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'build')
+  ]
+
+  if (env && env.NODE_ENV === 'production') {
+    const dotenv = new Dotenv({ path: './.env.dev' })
+    plugins.push(dotenv)
   }
-})
+
+  return {
+    mode: 'development',
+    entry: {
+      app: './src/index.js'
+    },
+    plugins,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'build')
+    }
+  }
+}
